2aab2735ee6f59009f2edec3b43c5054baa534cd
Omar Waleed <99154111+omarwaleed7@users.noreply.github.com>
2022-12-30 20:20:45 +0200

    Add files via upload

---
+++ Discrete2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SECTION
+{
+    class FCI
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input numbers from");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("To");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Perfect numbers between this numbers is");
+            for(x<=y;x++)
+            {
+                int sum = 0;
+                for(int i =1;i<x;i++)
+                {
+                    if (x%i == 0)
+                    {
+                        sum += i;
+                    }
+                }
+                if(x==sum)
+                {
+                    Console.WriteLine(X);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ discrete 1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SECTION
+{
+    class FCI
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter first number");
+            int FIRSTNUMBER= int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the Second number");
+            int SECONDNUMBER = int.Parse(Console.ReadLine());
+            int i, sum;
+            for(int n=FIRSTNUMBER;n<=SECONDNUMBER;n++)
+            {
+                i = 1;
+                sum = 0;
+                while(i<n)
+                {
+                    if (n % i == 0)
+                        sum = sum + i;
+                    i++;
+                }
+                if (sum == n)
+                    Console.WriteLine("{0}", n);
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
